30904bc17646a69feddeb49f51ffb31b9855165f
Jaden Hawkes <jadenhawkes@gmail.com>
2023-03-31 10:44:51 -0600

    Starting ups

---
+++ keeper.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'hawkes-k.us.auth0.com'
+export const clientId = 'E16Sh8x2ye2tLTvdiMHccd6Awzk5lodG'
+export const audience = 'https://cwhk-api.com'
